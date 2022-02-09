359b0ee24276d67e584a487c96aa60c53dcf44c3
aburizalpurnama <purnamar9@gmail.com>
2022-02-09 22:52:51 +0700

    Setup Project

---
+++ src/main/java/com/rizalpurnama/soaltestprogrammer/SoalTestProgrammerApplication.java
@@ -0,0 +1,13 @@
+package com.rizalpurnama.soaltestprogrammer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoalTestProgrammerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoalTestProgrammerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rizalpurnama/soaltestprogrammer/SoalTestProgrammerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rizalpurnama.soaltestprogrammer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoalTestProgrammerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
